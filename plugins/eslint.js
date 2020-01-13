83b6255b06dc314bbf5df451ebb52a61a7ac9311
Aqui TSUCHIDA <aqui.tcd+dev@gmail.com>
2020-01-13 11:04:46 +0900

    init

---
+++ plugins/eslint.js
@@ -0,0 +1,15 @@
+const { resolve } = require(`path`)
+
+function EsLinterPlugin(api) {
+  api.chainWebpack(config => {
+    config.module
+      .rule(`es-lint`)
+      .test(/\.(js|vue)$/)
+      .pre()
+      .use()
+      .loader(`eslint-loader`)
+      .options({ failOnError: true })
+  })
+}
+
+module.exports = EsLinterPlugin
